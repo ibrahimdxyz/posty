068b52c13e20fe4057304d34be76887a6720c65c
Ibrahim Aldarra <ibrahim.aldarra@student.hu.nl>
2021-03-24 09:17:28 +0100

    hide post form from guests

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,25 +3,27 @@
 @section('contents')
     <div class="flex justify-center">
         <div class="w-10/12 flex-col m-7 pb-6">
-            <form action="{{ route('posts') }}" method="POST" class="p-6 bg-gray-200 rounded-xl border border-gray-300">
-                @csrf
-                <div>
-                    <label for="body"></label>
-                    <textarea name="body" id="body" cols="30" rows="5" class="border-2 p-4 rounded-lg w-full
-                    @error('body') border-red-500 border-3 @enderror" placeholder="Write a post.."></textarea>
+            @auth
+                <form action="{{ route('posts') }}" method="POST" class="p-6 bg-gray-200 rounded-xl border border-gray-300">
+                    @csrf
+                    <div>
+                        <label for="body"></label>
+                        <textarea name="body" id="body" cols="30" rows="5" class="border-2 p-4 rounded-lg w-full
+                        @error('body') border-red-500 border-3 @enderror" placeholder="Write a post.."></textarea>
 
-                    @error('body')
-                        <div class="text-red-500 mt-2 text-small">
-                            {{ $message }}
-                        </div>    
-                    @enderror
-                </div>
-                <div class="flex justify-end">
-                    <button type="submit" class="bg-blue-500 text-white px-12 py-2 mt-4 rounded font-medium hover:bg-opacity-75">
-                        Post
-                    </button>
-                </div>
-            </form>
+                        @error('body')
+                            <div class="text-red-500 mt-2 text-small">
+                                {{ $message }}
+                            </div>    
+                        @enderror
+                    </div>
+                    <div class="flex justify-end">
+                        <button type="submit" class="bg-blue-500 text-white px-12 py-2 mt-4 rounded font-medium hover:bg-opacity-75">
+                            Post
+                        </button>
+                    </div>
+                </form>
+            @auth
 
 
             <div class="flex justify-center">
